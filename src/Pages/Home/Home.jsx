681151476d89d2902e5c9305e9a18af9fcbf898f
Henry Gillard <henry@MacBook-Pro.local>
2021-11-18 16:17:16 -0800

    css

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,6 +2,16 @@ import './Home.css'
 
 export default function Home() {
     return(
-        <h1>Home</h1>
+        <section>
+            <h1>Hello and Welcome!</h1>
+            <div className="main-text-container">
+                <h3>
+                    An expert in treating patients with substance 
+                    use disorder (SUD), Holly McCravey is a leader in
+                    correctional health, adult care, and 
+                    community-based programs.
+                </h3>
+            </div>
+        </section>
     )
 }
